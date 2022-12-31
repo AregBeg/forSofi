30c0ea836eef3cbe62c2f96c34df0ceff5c99be2
AregBeg <beg.areg1255@gmail.com>
2022-12-31 20:56:40 +0400

    some changes

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
-const namak = document.querySelector('.left_block');
-const pesa = document.querySelector('.pesa');
-const flower_left = document.querySelector('.to_left_flower');
-const flower_right = document.querySelector('.to_right_flower');
-const namesHarsUPesa = document.querySelector('.names p');
-const names = document.querySelector('.names');
-const pak_srtik = document.querySelector('.pak_srtik')
-const right_block = document.querySelector('.right_block')
+const windowSize = window.innerWidth;
+let namak, pesa, names, right_block;
+
+if (windowSize > 1000) {
+    document.querySelector('body').removeChild(document.querySelector('.small_container'))
+    namak = document.querySelector('.left_block');
+    pesa = document.querySelector('.pesa');
+    names = document.querySelector('.names');
+    right_block = document.querySelector('.right_block')
+} else {
+    document.querySelector('body').removeChild(document.querySelector('.container'))
+    namak = document.querySelector('.left_block');
+    pesa = document.querySelector('.pesa');
+    names = document.querySelector('.names');
+    right_block = document.querySelector('.right_block')
+}
+
 const namesHideHandler = () => {
     names.classList.add('hide_names_half');
-    // setTimeout(() => {
-    //     flower_right.style.display = 'none';
-    //     flower_left.style.display = 'none';
-    //     namesHarsUPesa.style.display = 'none';
-    //     pak_srtik.style.display = 'none';
-    // },1000)
-    setTimeout(() => {
-        right_block.classList.add('show_right_half')
-    }, 1000)
+    right_block.classList.add('show_right_half')
 }
 const pesaHandler = () => {
     pesa.classList.add('pesu_qaylel');
-    setTimeout(() => {
-        namesHideHandler();
-    }, 2500)
+    namesHideHandler();
 }
 namak.addEventListener('click', pesaHandler);
